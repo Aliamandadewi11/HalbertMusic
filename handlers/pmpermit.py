81d2a276343be63fafa5df02c74ebf87ed0355b6
ItsmeVipersz <83196949+ItsmeVipersz@users.noreply.github.com>
2021-04-27 07:55:50 +0700

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Alooo Guys ✨ Saya adalah **Layanan Asisten Star Music Bot.**\n\n ❗️ Rules :\n   - Biasakan Salam, amanat dari [Owner](t.me/ItsmeAlsya) \n   - Jangan spam pemutaran lagu agar bot tidak error \n\n **Note :** Kirim link undangan Grup atau Username Grup jika Userbot/Asisten tidak dapat bergabung dengan Grup Anda atau yang lainnya.\n\n 🛠️ Ke [Official Group](t.me/Republicfriend) atau bisa PC ke owner [Rezy](t.me/ItsmeAlsya)\n\n")
+  await USER.send_message(message.chat.id,"Alooo Guys ✨ Saya adalah **Layanan Asisten Star Music Bot.**\n\n ❗️ Rules :\n   - Biasakan Salam, amanat dari [Owner](t.me/ItsmeAlsya) \n   - Jangan spam pemutaran lagu agar bot tidak error \n\n **Note :** Kirim link undangan Grup atau Username Grup jika Userbot/Asisten tidak dapat bergabung dengan Grup Anda atau yang lainnya.\n\n 🛠️ Ke [Official Group](t.me/Republicfriend) atau bisa PC ke owner [Rezy](t.me/ItsmeAlsya).Thanks\n\n")
   return                        
